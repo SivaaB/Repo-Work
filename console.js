1cd1c2c531a0755bba9bfd9e0871e28d4ac6ef78
Sivaa B <mmailsivaa@gmail.com>
2022-10-21 22:03:37 +0400

    changes something
    okok
    
    Co-Authored-By: alex123849 <116372508+alex123849@users.noreply.github.com>

--- console.js
+++ console.js
@@ -1,9 +1,6 @@
-console.log("Hello World!")
-console.log("Hello World!!") // 2nd time; 
+console.log("Hello World!"); 
 
 function main () { 
-return null; 
-} 
+console.log(1); 
+}
 main()
-
-console.log(main) 
